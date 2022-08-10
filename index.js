92f55a00df21f8a4f0cebe782fb321a595d369dd
rakesh-pilly <rakesh.mudhiraj882@gmail.com>
2022-08-10 20:03:25 +0530

    checking with heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res, next) => {
-  res.send("backend is live for lms! Happy coding");
+  res.status(200).json("backend is live for lms! Happy coding");
 });
 
 
